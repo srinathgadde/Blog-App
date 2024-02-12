6c319712db3182fde772ca786d6c3978269a7ff2
srinathgadde <srinathgaddde@gmail.com>
2024-02-12 15:43:16 +0530

    changes

--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { UseDispatch, useDispatch } from "react-redux";
+import { useDispatch } from "react-redux";
 import authService from "../../appwrite/config";
 import { logout } from "../../store/authSlice";
 
@@ -13,6 +13,7 @@ function LogoutBtn() {
 
   return (
     <button
+      onClick={logoutHandler}
       className="inline-bock px-6 py-2 duration-200
   hover:bg-blue-100 rounded-full"
     >
